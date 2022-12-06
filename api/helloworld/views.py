752fdea0bbd31b54f92c7b9cfedc9ee0bcfd16b3
tqe1999 <tqe1999@gmail.com>
2022-12-05 21:46:25 -0500

    add logging, API key, exponential backoff retries

--- api/helloworld/views.py
+++ api/helloworld/views.py
@@ -1,16 +1,22 @@
 from django.http import HttpResponse, FileResponse, JsonResponse, HttpResponseBadRequest
 from helloworld.correlation import *
 from helloworld.models import Stocks
+from helloworld.mod_log import logger
 from django.views.decorators.csrf import csrf_exempt
 import json
+import time
 
-stocks = list(set(Stocks.objects.values_list('stock', flat=True)))
 
 def index(request):
     return HttpResponse("Hello, world!")
 
 # This endpoint returns a list of stocks
 def getStockList(request):
+    stocks = []
+    try:
+        stocks = list(set(Stocks.objects.values_list('stock', flat=True)))
+    except Exception as e:
+        logger.warning(f"request failed with exception {e}")
     return JsonResponse(list(stocks), safe=False)
 
 # This endpoint returns an image, given the name of two stocks
@@ -19,19 +25,23 @@ def getCorrelationGraph(request):
     
     stocks = []
 
-    if request.method == "POST":
-        print(request.POST)
-        stocks = json.loads(request.POST["stocks"])
-    elif request.method == "GET":
-        stocks.append(request.GET["stock1"])
-        stocks.append(request.GET["stock2"])
-    else:
+    try:
+        if request.method == "POST":
+            print(request.POST)
+            stocks = json.loads(request.POST["stocks"])
+        elif request.method == "GET":
+            stocks.append(request.GET["stock1"])
+            stocks.append(request.GET["stock2"])
+    except Exception as e:
+        logger.warning(f"request failed with bad request {e}")
         return HttpResponseBadRequest()
 
+    try:
+        df = get_df(stocks)
+        corr_df = get_correlation(df)
+        img = render_matrix(corr_df)
+        response = FileResponse(img, content_type="image/png")
+    except Exception as e:
+        logger.warning(f"rendering matrix failed with exception {e}")
 
-    df = get_df(stocks)
-    corr_df = get_correlation(df)
-    img = render_matrix(corr_df)
-
-    response = FileResponse(img, content_type="image/png")
     return response
